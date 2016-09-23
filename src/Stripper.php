14943988f134da53ca4644cae21e0c348e66c1e3
mohsen ranjbar <mimrahe@gmail.com>
2016-09-23 22:09:11 +0330

    complete list of html tags

--- src/Stripper.php
+++ src/Stripper.php
@@ -29,16 +29,38 @@ class Stripper
     protected function tags()
     {
         return [
-            'br',
-            'body',
-            'html',
-            'head',
-            'meta',
-            'link',
-            'strong',
-            'b',
-            'a',
-            'p'
+            // basic
+            '!DOCTYPE', 'html', 'title', 'body',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'p', 'br', 'hr',
+            // formatting
+            'acronym', 'abbr', 'address', 'b', 'bdi', 'bdo', 'big',
+            'blockquote', 'center', 'cite', 'code', 'del', 'dfn', 'em',
+            'font', 'i', 'ins', 'kbd', 'mark', 'meter', 'pre', 'progress',
+            'q', 'rp', 'rt', 'ruby', 's', 'samp', 'small', 'strike', 'strong',
+            'sub', 'sup', 'time', 'tt', 'u', 'var', 'wbr',
+            // forms and input
+            'form', 'input', 'textarea', 'button', 'select', 'optgroup', 'option',
+            'label', 'fieldset', 'legend', 'datalist', 'keygen', 'output',
+            // frames
+            'frame', 'frameset', 'noframes', 'iframe',
+            // images
+            'img', 'map', 'area', 'canvas', 'figcaption', 'figure',
+            // audio and video
+            'audio', 'source', 'track', 'video',
+            // links
+            'a', 'link', 'nav',
+            // lists
+            'ul', 'ol', 'li', 'dir', 'dl', 'dt', 'dd', 'menu', 'menuitem',
+            // tables
+            'table', 'caption', 'th', 'tr', 'td', 'thead', 'tbody', 'tfoot', 'col', 'colgroup',
+            // styles and semantics
+            'style', 'div', 'span', 'header', 'footer', 'main', 'section', 'article',
+            'aside', 'details', 'dialog', 'summary',
+            // meta info
+            'head', 'meta', 'base', 'basefont',
+            // programming
+            'script', 'noscript', 'applet', 'embed', 'object', 'param'
         ];
     }
 
