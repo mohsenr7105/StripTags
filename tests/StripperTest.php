e1e66ebdc0971a750886138250e987c639071a09
mohsen ranjbar <mimrahe@gmail.com>
2016-09-24 11:19:23 +0330

    change naming convention

--- tests/StripperTest.php
+++ tests/StripperTest.php
@@ -6,24 +6,24 @@ class StripperTest extends TestCase
 {
     public $text = '<strong>strong text</strong><b>bold text</b><br><a href="#">link</a><p>paragraph</p>';
 
-    public function testExcept()
+    public function testOnly()
     {
         $exceptedTags = array(
             'b', 'strong'
         );
         $stripper = new Stripper($this->text);
-        $strippedText = $stripper->except($exceptedTags)->strip();
+        $strippedText = $stripper->only($exceptedTags)->strip();
         $this->assertEquals('strong textbold text<br><a href="#">link</a><p>paragraph</p>', $strippedText);
     }
 
-    public function testAllow()
+    public function testExcept()
     {
         $allowedTags = array(
             'br', 'strong', 'a'
         );
         $stripper = new Stripper();
         $stripper->text($this->text);
-        $stripper->allow($allowedTags);
+        $stripper->except($allowedTags);
         $strippedText = $stripper->strip();
         $this->assertEquals('<strong>strong text</strong>bold text<br><a href="#">link</a>paragraph', $strippedText);
     }
